961c69d92970911d7d478cd5bd3bc252c392c652
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-06-03 23:00:03 +0300

    Reviewed my code on Libre Office writer

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const names = ["Guadalupe","Ollie","Aki"];
+const arr = [];
+function writeCards(names) {
+  for (let i = 0; i < names.length; i++) {
+    arr.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+
+  }
+  return arr;
+}
+
+
+
+let num = 10;
+function countDown(num){
+    
+    while (num >= 0) {
+    console.log(num--);
+ }
+}
+
+countDown (num);
